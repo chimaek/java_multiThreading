19add69a9e7f651435a354be01f5f90d50005850
seungju.kim <pipiru100@gmail.com>
2024-09-07 10:36:10 +0900

    synchronized 사용법-1

--- src/thread_creation/example4/Main2.java
+++ src/thread_creation/example4/Main2.java
@@ -36,15 +36,15 @@ public class Main2 {
   private static class InventoryCounter {
     private int items = 0;
 
-    public void increment() {
+    public synchronized void increment() {
       items++;
     }
 
-    public void decrement() {
+    public synchronized void decrement() {
       items--;
     }
 
-    public int getItems() {
+    public synchronized int getItems() {
       return items;
     }
   }
---
+++ src/thread_creation/example5/Main.java
@@ -0,0 +1,8 @@
+package thread_creation.example5;
+
+public class Main {
+
+    public static void main(String[] args){
+
+    }
+}
